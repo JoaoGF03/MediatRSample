a63f439257f4c7bc843ad7b1911311341badad27
guilherme <joao.g.f@icloud.com>
2022-11-17 16:10:21 -0300

    Mediator Pattern

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+namespace MediatRSample.Repositories
+{
+  public interface IRepository<T>
+  {
+    Task<IEnumerable<T>> GetAll();
+
+    Task<T> Get(int id);
+
+    Task<T> Add(T item);
+
+    Task Edit(T item);
+
+    Task Delete(int id);
+  }
+}
